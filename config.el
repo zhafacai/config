e7d4587f40f03409d3d58ee565e972cd2433b51f
zhafacai <zhafacai@gmail.com>
2026-03-05 23:50:57 +0800

    I do not need big heading

--- config.el
+++ config.el
@@ -200,26 +200,6 @@
 (use-package modus-themes
   :demand t
   :init
-  ;; Starting with version 5.0.0 of the `modus-themes', other packages
-  ;; can be built on top to provide their own "Modus" derivatives.
-  ;; For example, this is what I do with my `ef-themes' and
-  ;; `standard-themes' (starting with versions 2.0.0 and 3.0.0,
-  ;; respectively).
-  ;;
-  ;; The `modus-themes-include-derivatives-mode' makes all Modus
-  ;; commands that act on a theme consider all such derivatives, if
-  ;; their respective packages are available and have been loaded.
-  ;;
-  ;; Note that those packages can even completely take over from the
-  ;; Modus themes such that, for example, `modus-themes-rotate' only
-  ;; goes through the Ef themes (to this end, the Ef themes provide
-  ;; the `ef-themes-take-over-modus-themes-mode' and the Standard
-  ;; themes have the `standard-themes-take-over-modus-themes-mode'
-  ;; equivalent).
-  ;;
-  ;; If you only care about the Modus themes, then (i) you do not need
-  ;; to enable the `modus-themes-include-derivatives-mode' and (ii) do
-  ;; not install and activate those other theme packages.
   (modus-themes-include-derivatives-mode 1)
   :bind
   (("<f5>" . modus-themes-rotate)
@@ -237,37 +217,16 @@
         modus-themes-prompts '(bold)
         modus-themes-headings
         '(
-	  ;; (agenda-structure . (variable-pitch light 2.2))
+          ;; (agenda-structure . (variable-pitch light 2.2))
           ;; (agenda-date . (variable-pitch regular 1.3))
-          (t . (regular 1.15))))
+	  ;; (t . (regular 1.15))
+	  ))
 
-  (setq modus-themes-common-palette-overrides nil)
-
-  ;; Finally, load your theme of choice (or a random one with
-  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
-  ;; `modus-themes-load-random-light').
-  ;; (modus-themes-load-theme 'modus-operandi-tinted)
-  )
+  (setq modus-themes-common-palette-overrides nil))
 
 (use-package ef-themes
   :init
-  ;; This makes the Modus commands listed below consider only the Ef
-  ;; themes.  For an alternative that includes Modus and all
-  ;; derivative themes (like Ef), enable the
-  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
-  ;; the Ef themes has a section that explains all the possibilities:
-  ;;
-  ;; - Evaluate `(info "(ef-themes) Working with other Modus themes or taking over Modus")'
-  ;; - Visit <https://protesilaos.com/emacs/ef-themes#h:6585235a-5219-4f78-9dd5-6a64d87d1b6e>
-  (ef-themes-take-over-modus-themes-mode 1)
-  :bind
-  (("<f5>" . modus-themes-rotate)
-   ("C-<f5>" . modus-themes-select)
-   ("M-<f5>" . modus-themes-load-random))
-  :config
-  ;; All customisations here.
-  (setq modus-themes-mixed-fonts t)
-  (setq modus-themes-italic-constructs t))
+  (ef-themes-take-over-modus-themes-mode 1))
 
 (set-face-attribute 'default nil
                     :family "Iosevka SS02"
