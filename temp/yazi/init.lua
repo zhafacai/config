60292814862a95217e43e212edd20c4c5ecbcc80
zhafacai <zhafacai@gmail.com>
2026-04-29 11:33:11 +0800

    add yazi config

---
+++ temp/yazi/init.lua
@@ -0,0 +1,20 @@
+require("git"):setup({
+	-- Order of status signs showing in the linemode
+	order = 1500,
+})
+require("bunny"):setup({
+	hops = {
+		{ key = "/", path = "/" },
+		{ key = "t", path = "/tmp" },
+		{ key = "~", path = "~", desc = "Home" },
+		{ key = "c", path = "~/.config", desc = "Config files" },
+		{ key = { "l", "s" }, path = "~/.local/share", desc = "Local share" },
+		{ key = { "l", "b" }, path = "~/.local/bin", desc = "Local bin" },
+		{ key = { "l", "t" }, path = "~/.local/state", desc = "Local state" },
+	},
+	desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+	ephemeral = true, -- Enable ephemeral hops, default is true
+	tabs = true, -- Enable tab hops, default is true
+	notify = false, -- Notify after hopping, default is false
+	fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})
