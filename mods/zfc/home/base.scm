429ce83485f1ef9a48b904fc5a29adc6cf205e33
zhafacai <zhafacai@gmail.com>
2026-05-30 15:12:22 +0800

    use pinentry gnome

--- mods/zfc/home/base.scm
+++ mods/zfc/home/base.scm
@@ -167,7 +167,7 @@
                         (list fcitx5-rime))))
              (service home-gpg-agent-service-type
                       (home-gpg-agent-configuration
-                       (pinentry-program (file-append pinentry-rofi "/bin/pinentry-rofi"))
+                       (pinentry-program (file-append pinentry-gnome3 "/bin/pinentry-gnome3"))
                        (default-cache-ttl 1800)
                        (extra-content "allow-loopback-pinentry")
                        (ssh-support? #t)))
